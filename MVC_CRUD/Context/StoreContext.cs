13f61c48d06dac5d536eb4217e724562597abcf8
Adria <adriangaray396@gmail.com>
2019-09-08 23:54:55 -0300

    Se vuelve agregar los archivos correctos

---
+++ MVC_CRUD/Context/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_CRUD.Context
+{
+    public class StoreContext:DbContext
+    {
+        public DbSet<Models.Product> Products { get; set; }
+    }
+}
